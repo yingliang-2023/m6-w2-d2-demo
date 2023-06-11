f1c1889e37e7599e7974287f4e327d4e2032e981
YING LIANG <y.liang.168@westcliff.edu>
2023-06-11 15:14:44 -0500

    commmit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Header from './features/header/Header';
+import Footer from './features/footer/Footer';
+import TodoList from './features/todos/TodoList'
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <section>
+          <h1>Redux Fundamentals Example</h1>
+        </section>
+      </nav>
+
+      <main>
+        <section className='medium-container'>
+          <h2>Todos</h2>
+          <div className='todoapp'>
+            <Header />
+            <TodoList />
+            <Footer />
+          </div>
+        </section>
+      </main>
+
+      <section>
+        <h2>Welcome to the Redux Fundamentals example app!</h2>
+      </section>
+    </div>
+  )
+}
+
+export default App
